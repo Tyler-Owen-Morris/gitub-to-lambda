a7f2b3deb32e4191e779269d447d59ceeb3987c6
flicknewb <theowenmorris@gmail.com>
2022-05-15 10:12:46 -0700

    connecting S3 to box

--- lambda_function.py
+++ lambda_function.py
@@ -4,27 +4,19 @@ from random import randint
 import os
 import csv
 import pandas as pd
-import boto3
 from io import StringIO
 import requests
-from boxsdk import OAuth2, Client, JWTAuth
+import boto3
+from boxsdk import OAuth2, Client
 
 ## **** CONFIGURATION VARIABLES **** ##
-# DB ENDPOINT
-endpoint = os.environ['DB_DOMAIN']
-username = os.environ['DB_USERNAME']
-password = os.environ['DB_PASS']
-database_name = os.environ['DB_NAME']
-
-## tables to be updated
-survey_tbl = "detention_survey_tbl"
-#suspension_tbl = "tbl_suspended_visitations"
 bucket = 'empact-test'
 
-# DB CONNECTION OBJECT INITILIZATION
-connection = pymysql.connect(
-    host=endpoint, user=username, passwd=password, db=database_name
-)
+# S3 INIT
+s3 = boto3.resourse('s3')
+my_bucket = s3.Bucket(bucket)
+
+
 
 # box sdk INIT
 auth = OAuth2(
@@ -32,21 +24,6 @@ auth = OAuth2(
     client_secret = os.environ['BOX_CLIENT_SECRET'],
     access_token = os.environ['BOX_DEV_TOKEN'],
 )
-
-""" auth_settings = {
-    "boxAppSettings": {
-        "clientID": os.environ['BOX_CLIENT_ID'],
-        "clientSecret": os.environ['BOX_CLIENT_SECRET'],
-        "appAuth": {
-        "publicKeyID": os.environ['BOX_JWT_AUTH_PUBLIC_KEY'],
-        "privateKey": os.environ['BOX_JWT_AUTH_PRIVATE_KEY'],
-        "passphrase": os.environ['BOX_JWT_AUTH_PASSPHRASE']
-        }
-    },
-    "enterpriseID": os.environ['BOX_ENTERPRISE_ID']
-}
-
-auth = JWTAuth.from_settings_dictionary(auth_settings) """
 
 client = Client(auth)
 
@@ -59,23 +36,10 @@ print(f'The current user ID is {user.id}')
 def lambda_handler(event, context):
     
     print("i'm alive!")
-    print("JWTAuth object:",JWTAuth)
-    print(connection)
-    cursor = connection.cursor(pymysql.cursors.DictCursor)
-    query = "SELECT * FROM `"+database_name+"`."+survey_tbl
-    cursor.execute(query)
-    s_rows = cursor.fetchall()
-    print(len(s_rows))
-    df = pd.DataFrame(s_rows)
-    print(df.head())
-    print(df.columns)
-    sites = df['SiteID'].unique()
-    for site in sites:
-        sav_df = df.loc[df['SiteID'] == site]
-        print(sav_df.head())
-        ## Write to services section 
-        #write_to_S3(site,sav_df)
-        write_to_box(site,sav_df)
+    
+    for buck in my_bucket.objects.all():
+        print(buck.key)
+
     message = {"message": "Execution started successfully!"}
     return {
         "statusCode": 200,
@@ -84,12 +48,6 @@ def lambda_handler(event, context):
     }
 
 ## UTILITY FUNCTIONS
-def write_to_S3(sit,s_df):
-    csv_buffer = StringIO()
-    s_df.to_csv(csv_buffer)
-    s3_resource = boto3.resource('s3')
-    s3_resource.Object(bucket, sit+'.csv').put(Body=csv_buffer.getvalue())
-
 def write_to_box(fname,df):
     folders = getAllFolders()
     fold_names = [x[0] for x in folders]
@@ -168,3 +126,5 @@ def updateFile(fileid,file_name,data):
     updated_file = client.file(fileid).update_contents_with_stream(stream)
     print(f'File "{updated_file.name}" has been updated')
     return updated_file
+
+lambda_handler()
