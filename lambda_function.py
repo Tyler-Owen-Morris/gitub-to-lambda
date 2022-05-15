7847492ca225b6ea7961e9ccddefcb864c847428
flicknewb <theowenmorris@gmail.com>
2022-05-15 13:13:00 -0700

    attempting to write files from storage to box

--- lambda_function.py
+++ lambda_function.py
@@ -211,8 +211,8 @@ def update_box_pdf(fileid,file_path):
 
 def write_pdf_to_box(folderid, file_name,file_path):
     # Prepare example
-    with open(file_path, "rb") as fh:
-        bytes_stream = BytesIO(fh.read())
+    fh = open(file_path, "rb") 
+    bytes_stream = BytesIO(fh.read())
     # Read from bytes_stream
     reader = PdfFileReader(bytes_stream)
     # Write to bytes_stream
