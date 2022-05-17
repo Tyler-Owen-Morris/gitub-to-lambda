f6a6d3b4b0fc91fe4aa1782f4564f7b0536faf2f
flicknewb <theowenmorris@gmail.com>
2022-05-16 17:01:55 -0700

    adding ability to delete source files automatically

--- lambda_function.py
+++ lambda_function.py
@@ -36,7 +36,6 @@ print(f'The current user ID is {user.id}')
 # AWS LAMBDA SETUP TARGETS THE 'lambda_handler' FUNCTION IN THE 'lambda_function.py' FILE.
 ## This is the entry point for the API endpoint being called.
 def lambda_handler(event, context):
-    
     print("i'm alive!")
     myfiles,myfolders = getAllEntities() #2 lists of tuples (file name, file box id)
     f_list = read_from_s3() #string list of file objects from S3 .key is file name, .bucket is location
@@ -49,7 +48,7 @@ def lambda_handler(event, context):
                 resp = my_bucket.download_file(fil.key,'/tmp/'+fil.key)
                 print("download response:?",resp)
                 saved_files.append(('/tmp/'+fil.key,fil.key))
-                s3.Object(bucket,fil.key).delete()
+                # s3.Object(bucket,fil.key).delete()
             except ValueError as ve:
                 print("ERROR:",ve)
                 print("download failed for:",fil)
@@ -82,7 +81,7 @@ def lambda_handler(event, context):
                         fid = folder[1]
                 print("write new file to box:",fid,f_key,f_path)
                 #write_pdf_to_box(fid, f_key,f_path)
-                write_bin_to_box(fid,f_key,f_path)
+                write_pdf_to_box(fid,f_key,f_path)
             else:
                 print("No matching folder or file found for:",ms_file)
 
@@ -123,35 +122,6 @@ def getAllEntities():
                 #folders.append((item.name,item.id))
     return files,folders
 
-def write_to_box(fname,df):
-    folders = getAllFolders()
-    fold_names = [x[0] for x in folders]
-    fold_id = None
-    # create folder
-    if fname in fold_names:
-        for folder in folders:
-            if folder[0] == fname:
-                fold_id = folder[1]
-    else:
-        fold_id = createFolder(fname)
-    # create file in folder
-    print("foldID:",fold_id)
-    if (fold_id == None or fold_id == ''):
-        return
-    files = getAllFiles(str(fold_id))
-    file_names = [x[0] for x in files]
-    print("file names:",file_names)
-    if fname + ".csv" in file_names:
-        file_id = None
-        for file in files:
-            if file[0] == fname+".csv":
-                file_id = file[1]
-        print("updating file", file_id,fname)
-        updateFile(file_id,fname,df)
-    else:
-        createFile(fold_id,fname,df)
-    pass
-
 def getAllFolders():
     folders = []
     items = client.folder('0').get_items(limit=1000)
@@ -176,32 +146,6 @@ def getAllFiles(folder='0'):
     print(files)
     return files
 
-def createFolder(new_folder_name, parent_folder = client.folder('0'), ):
-    fid = ''
-    try:
-        new_folder = parent_folder.create_subfolder(new_folder_name)
-        fid = new_folder.id
-    except:
-        folders = getAllFolders()
-        for fold in folders:
-            print("Matching:",fold,new_folder_name)
-            if fold[0] == new_folder_name:
-                fid = fold[1]
-    return fid
-
-def createFile(folderid, file_name,data):
-    stream = StringIO()
-    # data.to_csv(stream)
-    fname = file_name
-    box_file = client.folder(folderid).upload_stream(stream, fname)
-
-def updateFile(fileid,file_name,data):
-    stream = StringIO()
-    data.to_csv(stream)
-    updated_file = client.file(fileid).update_contents_with_stream(stream)
-    print(f'File "{updated_file.name}" has been updated')
-    return updated_file
-
 def update_box_pdf(fileid,file_path):
     with open(file_path, "rb") as fh:
         bytes_stream = BytesIO(fh.read())
@@ -209,20 +153,7 @@ def update_box_pdf(fileid,file_path):
         print(f'File "{updated_file.name}" has been updated')
     return updated_file
 
-def write_pdf_to_box(folderid, file_name,file_path):
-    stream = StringIO()
-    pdfFileObj = open(file_path,'rb')
-    pdfReader = PdfFileReader(pdfFileObj)
-    for i in range(0,pdfReader.numPages):
-        pageObj = pdfReader.getPage(i)
-        extr_txt = pageObj.extractText()
-        print("extrTxt",extr_txt)
-        stream.write("hello world!")
-    stream.seek(0)
-    box_file = client.folder(folderid).upload_stream(stream,file_name.split(".")[0]+".txt")
-    print("created new file in box:",box_file)
-
-def write_bin_to_box(folderid,file_name,file_path):
+def write_pdf_to_box(folderid,file_name,file_path):
     with open(file_path, "rb") as fh:
         bytes_stream = BytesIO(fh.read())
         box_file = client.folder(folderid).upload_stream(bytes_stream,file_name)
